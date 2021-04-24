91cfb8e9a6a6e855a1952d828cf74b735a562e94
Pareja Escamilla <g.pareja.escamilla@accenture.com>
2021-04-24 18:58:53 +0200

    Deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCgj6TRJO9OnUqLMVqLsqXWeczzyil18cY',
+    authDomain: 'motospdf.firebaseapp.com',
+    projectId: 'motospdf',
+    storageBucket: 'motospdf.appspot.com',
+    messagingSenderId: '355640681304',
+    appId: '1:355640681304:web:5b80c2f4b3f079a9677b2d',
+    measurementId: 'G-30WFGYK8Z3'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCgj6TRJO9OnUqLMVqLsqXWeczzyil18cY",
-    authDomain: "motospdf.firebaseapp.com",
-    projectId: "motospdf",
-    storageBucket: "motospdf.appspot.com",
-    messagingSenderId: "355640681304",
-    appId: "1:355640681304:web:5b80c2f4b3f079a9677b2d",
-    measurementId: "G-30WFGYK8Z3"
+    apiKey: 'AIzaSyCgj6TRJO9OnUqLMVqLsqXWeczzyil18cY',
+    authDomain: 'motospdf.firebaseapp.com',
+    projectId: 'motospdf',
+    storageBucket: 'motospdf.appspot.com',
+    messagingSenderId: '355640681304',
+    appId: '1:355640681304:web:5b80c2f4b3f079a9677b2d',
+    measurementId: 'G-30WFGYK8Z3'
   }
 };
